a89c4def723dd283d1d813ab9f34ec3bfdf4bced
larkes <playegrovoy@gmail.com>
2022-06-16 14:12:03 +0300

    yep

--- app/src/main/java/com/example/StartApp/data/storage/database_for_goals/AppDatabase.kt
+++ app/src/main/java/com/example/StartApp/data/storage/database_for_goals/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.example.StartApp.data.storage.database_for_goals.dao.SomeDao
 import com.example.StartApp.data.storage.database_for_goals.entity.EntitySomeData
 
-
+//gfghfghfh
 @Database(entities = [EntitySomeData::class], version = 1)
 abstract class AppDatabase:RoomDatabase() {
     abstract fun someDao(): SomeDao
